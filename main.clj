011328d997df77d45b6a87649e63644d3c361c2f
Carsten Behring <carsten.behring@gmail.com>
2023-12-04 20:46:47 +0100

    added more files

---
+++ main.clj
@@ -0,0 +1,26 @@
+(require '[babashka.fs :as fs])
+(require '[clojure.java.io :as io])
+(require '[babashka.http-client :as http])
+
+;;
+(require '[babashka.cli :as cli])
+
+(def cli-options {:port {:default 80 :coerce :long}
+                  :help {:coerce :boolean}})
+
+
+(prn :args *command-line-args*)
+(prn (cli/parse-opts *command-line-args* {:spec cli-options}))
+
+;; (println "hello from github")
+;; (println :neil-opts @babashka.neil/*neil-opts*)
+;; (println :cwd (fs/cwd))
+
+(io/copy
+     (:body (http/get "https://github.com/behrica/neil-devcontainer-ext/raw/main/devcontainer.zip"
+                      {:as :stream}))
+     (io/file "/tmp/devcontainer.zip"))
+(fs/unzip "/tmp/devcontainer.zip" ".")
+
+(println ".devcontainer folder created")
+
