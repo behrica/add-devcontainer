6f921fc341db51f9700b9e05eb34481b1b0c808a
Carsten Behring <carsten.behring@gmail.com>
2023-12-09 11:15:24 +0100

    added poetry

--- src/behrica/adddev/devcontainer.clj
+++ src/behrica/adddev/devcontainer.clj
@@ -36,8 +36,9 @@
   [edn data]
 
   (let [features (merge clojure-features
-                        (if (:with-python data) {"ghcr.io/devcontainers/features/python:1"           {}} {})
-                        (if (:with-R data)      {"ghcr.io/rocker-org/devcontainer-features/r-apt:0"  {}} {}))
+                        (if (:with-python data) {"ghcr.io/devcontainers/features/python:1"          {}
+                                                 "ghcr.io/devcontainers-contrib/features/poetry:2"  {} } )
+                        (if (:with-R data)      {"ghcr.io/rocker-org/devcontainer-features/r-apt:0"  {}} ))
         deps (merge  base-deps
                      (if (:with-python data) {'clj-python/libpython-clj {:mvn/version "2.025"}} {})
                      (if (:with-R data) {'scicloj/clojisr {:mvn/version "1.0.0-BETA21"}} {}))]
